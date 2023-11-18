9ae9837f8704224d1ecc99be64deded82ce48ec1
lovis-ambrose <lovisambrose212@gmail.com>
2023-11-18 15:12:46 +0300

    Initial commit

---
+++ UI/app.js
@@ -0,0 +1,105 @@
+Dropzone.autoDiscover = false;
+
+function init() {
+    let dz = new Dropzone("#dropzone", {
+        url: "/",
+        maxFiles: 1,
+        addRemoveLinks: true,
+        dictDefaultMessage: "Some Message",
+        autoProcessQueue: false
+    });
+    
+    dz.on("addedfile", function() {
+        if (dz.files[1]!=null) {
+            dz.removeFile(dz.files[0]);        
+        }
+    });
+
+    dz.on("complete", function (file) {
+        let imageData = file.dataURL;
+        
+        var url = "http://127.0.0.1:4000/classify_image";
+
+        $.post(url, {
+            image_data: file.dataURL
+        },function(data, status) {
+            /* 
+            Below is a sample response if you have two faces in an image lets say virat and roger together.
+            Most of the time if there is one person in the image you will get only one element in below array
+            data = [
+                {
+                    class: "elon_musk",
+                    class_probability: [1.05, 12.67, 22.00, 4.5, 91.56],
+                    class_dictionary: {
+                        'elon_musk': 0,
+                        'larry_ellison': 1,
+                        'satya_nandella': 2,
+                        'marissa_mayer': 3,
+                        'jeff_bezoz': 4,
+                        'steve_jobs': 5,
+                        'sundar_pichai': 6
+                    }
+                },
+                {
+                    class: "roder_federer",
+                    class_probability: [7.02, 23.7, 52.00, 6.1, 1.62],
+                    class_dictionary: {
+                        'elon_musk': 0,
+                        'larry_ellison': 1,
+                        'satya_nandella': 2,
+                        'marissa_mayer': 3,
+                        'jeff_bezoz': 4,
+                        'steve_jobs': 5,
+                        'sundar_pichai': 6
+                    }
+                }
+            ]
+            */
+            console.log(data);
+            if (!data || data.length==0) {
+                $("#resultHolder").hide();
+                $("#divClassTable").hide();                
+                $("#error").show();
+                return;
+            }
+            let tech_person = ["elon_musk", "jeff_bezoz", "larry_ellison", "marissa_mayer", "satya_nandella"];
+            
+            let match = null;
+            let bestScore = -1;
+            for (let i=0;i<data.length;++i) {
+                let maxScoreForThisClass = Math.max(...data[i].class_probability);
+                if(maxScoreForThisClass>bestScore) {
+                    match = data[i];
+                    bestScore = maxScoreForThisClass;
+                }
+            }
+            if (match) {
+                $("#error").hide();
+                $("#resultHolder").show();
+                $("#divClassTable").show();
+                $("#resultHolder").html($(`[data-player="${match.class}"`).html());
+                let classDictionary = match.class_dictionary;
+                for(let personName in classDictionary) {
+                    let index = classDictionary[personName];
+                    let proabilityScore = match.class_probability[index];
+                    let elementName = "#score_" + personName;
+                    $(elementName).html(proabilityScore);
+                }
+            }
+            // dz.removeFile(file);            
+        });
+    });
+
+    $("#submitBtn").on('click', function (e) {
+        dz.processQueue();		
+    });
+}
+
+$(document).ready(function() {
+    console.log( "ready!" );
+    $("#error").hide();
+    $("#resultHolder").hide();
+    $("#divClassTable").hide();
+
+    init();
+});
